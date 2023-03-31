bddbbfb1b1cf5e6c7425ba014a01ce1574fa8327
Paul Nwabudike <chindinma@gmail.com>
2023-03-31 11:30:04 +0100

    Add Factory Pattern

---
+++ src/com/patterns/creational/factory/Animal.java
@@ -0,0 +1,16 @@
+/**
+ * @author Paul Nwabudike
+ * chindinma@gmail.com
+ * Since: 11:21:07 AM Mar 31, 2023
+ * Copyright (c) 2023.  
+ */
+package com.patterns.creational.factory;
+
+/**
+ * design-patterns-java/com.patterns.creational.factory/Animal.java/Animal
+ * 
+ */
+public interface Animal {
+	void displayBehaviour();
+
+}
